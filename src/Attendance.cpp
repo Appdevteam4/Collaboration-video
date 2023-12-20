48ea7e30ffcfdc8add7cab179cc48f7f714b699a
Magic <2227726@slu.edu.ph>
2023-12-20 16:51:20 +0800

    Merge calc_time_5 and calc_time_15 into a unified calc_time function, introducing a minutes parameter. Refactor function parameters to const references for enhanced readability and maintainability. Adjust formatting and comments to align with Google C++ coding standards, improving overall code quality.

--- src/Attendance.cpp
+++ src/Attendance.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
-#include <utility>
-#include <vector>
-#include <chrono>
 #include <sstream>
 #include <string>
+#include <vector>
+#include <chrono>
 #include <ctime>
 
 using namespace std;
@@ -14,20 +13,21 @@ using matrix = vector<vec>;
 
 //======================================================================
 
-matrix readCSV( string filename )
+matrix readCSV(const string& filename)
 {
     char separator = ',';
     matrix result;
     string row, item;
 
-    ifstream in( filename );
+    ifstream in(filename);
 
-    while( getline( in, row ) )
+    while (getline(in, row))
     {
         vec R;
-        stringstream ss( row );
-        while ( getline ( ss, item, separator ) ) R.push_back( item );
-        result.push_back( R );
+        stringstream ss(row);
+        while (getline(ss, item, separator))
+            R.push_back(item);
+        result.push_back(R);
     }
 
     in.close();
@@ -36,75 +36,62 @@ matrix readCSV( string filename )
 
 //======================================================================
 
-void printMatrix( const matrix &M )
+void printMatrix(const matrix& M)
 {
-    for ( vec row : M )
+    for (const vec& row : M)
     {
-        for ( string s : row ) cout << setw( 22 ) << left << s << " ";    // need a variable format really
+        for (const string& s : row)
+            cout << setw(22) << left << s << " ";
         cout << '\n';
     }
 }
 
 //======================================================================
 
-string calc_time_5(string time)
+string calc_time(const string& time, int minutesToAdd)
 {
     size_t colon_pos = time.find(':');
     if (colon_pos != string::npos)
     {
         int hours = stoi(time.substr(0, colon_pos));
         int minutes = stoi(time.substr(colon_pos + 1));
-        // Add 5 minutes to the class schedule
-        minutes += 6;
+        minutes += minutesToAdd;
+
         if (minutes >= 60)
         {
             hours += 1;
             minutes -= 60;
         }
-        // Format the updated schedule
+
         stringstream updated_schedule;
         updated_schedule << setw(2) << setfill('0') << hours << ":" << setw(2) << setfill('0') << minutes;
-        string updated_schedule_str = updated_schedule.str();
-
-        return updated_schedule_str;
+        return updated_schedule.str();
     }
+    return ""; // Handle invalid time format
 }
-
 
 //======================================================================
 
-string calc_time_15(string time)
+string calc_time_5(const string& time)
 {
-    size_t colon_pos = time.find(':');
-    if (colon_pos != string::npos)
-    {
-        int hours = stoi(time.substr(0, colon_pos));
-        int minutes = stoi(time.substr(colon_pos + 1));
-        // Add 5 minutes to the class schedule
-        minutes += 16;
-        if (minutes >= 60)
-        {
-            hours += 1;
-            minutes -= 60;
-        }
-        // Format the updated schedule
-        stringstream updated_schedule;
-        updated_schedule << setw(2) << setfill('0') << hours << ":" << setw(2) << setfill('0') << minutes;
-        string updated_schedule_str = updated_schedule.str();
-
-        return updated_schedule_str;
-    }
+    return calc_time(time, 5);
 }
-
 
 //======================================================================
 
-string getCurrentDateTime() 
+string calc_time_15(const string& time)
+{
+    return calc_time(time, 15);
+}
+
+//======================================================================
+
+string getCurrentDateTime()
 {
     auto now = chrono::system_clock::now();
     time_t currentTime = chrono::system_clock::to_time_t(now);
     struct tm* localTimeInfo = localtime(&currentTime);
-    // localtime_s(&localTimeInfo, &currentTime); // Use localtime_s for Windows, use localtime for Linux
+
     char buffer[80];
     strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", localTimeInfo);
     return buffer;
@@ -112,7 +99,7 @@ string getCurrentDateTime()
 
 //======================================================================
 
-void add_date_to_output(matrix &M) 
+void add_date_to_output(matrix& M)
 {
     string currentDate = "Attendance Checked: " + getCurrentDateTime();
     vec newRow = {currentDate};
@@ -121,24 +108,21 @@ void add_date_to_output(matrix &M)
 
 //======================================================================
 
-void check_attendance(matrix &M)
-{   
-    // Assuming teacherTimeIn is in HH:MM format
+void check_attendance(matrix& M)
+{
     string class_schedule = M[1][0];
     string teacherTimeIn;
     cout << "Class Schedule: " << class_schedule << endl;
 
-    for (vec &row : M)
+    for (vec& row : M)
     {
         if (row.size() == 5 && row[2] == "Teacher")
         {
-            teacherTimeIn = row[4]; // Get the student's time in
+            teacherTimeIn = row[4];
             cout << "Updating attendance based on teacher's time in: " << teacherTimeIn << endl;
 
-            // Check if the teacher is absent or late
             if (teacherTimeIn.empty() || calc_time_15(class_schedule) < row[4])
             {
-                // Teacher is absent, set "N/A" for students
                 if (row[4] != "Time")
                     row[4] = "Absent";
             }
@@ -156,10 +140,9 @@ void check_attendance(matrix &M)
         else if (row.size() == 5)
         {
             string studentTimeIn = row[4];
-            // Teacher is on time, check student's time
+
             if (studentTimeIn.empty() || calc_time_15(teacherTimeIn) < studentTimeIn)
             {
-                // Student is absent or late
                 if (row[4] != "Time")
                     row[4] = "Absent";
             }
@@ -170,7 +153,6 @@ void check_attendance(matrix &M)
             }
             else
             {
-                // Student is on time
                 if (row[4] != "Time")
                     row[4] = "Present";
             }
@@ -181,16 +163,17 @@ void check_attendance(matrix &M)
 
 //======================================================================
 
-void writeCSV(const matrix &M, const string& filename)
+void writeCSV(const matrix& M, const string& filename)
 {
-    ofstream out(filename, ios::app); // Open the file in append mode
+    ofstream out(filename, ios::app);
+
     if (!out.is_open())
     {
         cerr << "Error: Unable to create file: " << filename << endl;
         return;
     }
 
-    for (const vec &row : M)
+    for (const vec& row : M)
     {
         for (size_t i = 0; i < row.size(); ++i)
         {
@@ -200,6 +183,7 @@ void writeCSV(const matrix &M, const string& filename)
         }
         out << '\n';
     }
+
     out.close();
 }
 
@@ -207,19 +191,19 @@ void writeCSV(const matrix &M, const string& filename)
 
 int main()
 {
-    matrix pets = readCSV( "test.csv" );
-    printMatrix( pets );
+    matrix pets = readCSV("test.csv");
+    printMatrix(pets);
 
     cout << "\n\n";
 
-    check_attendance( pets ); // check the attendance according to the time the person has timed in.
+    check_attendance(pets);
 
-    add_date_to_output(pets); // add the date the day the attendance was checked.
+    add_date_to_output(pets);
 
-    printMatrix( pets ); // show the checked attendance.
+    printMatrix(pets);
 
-    writeCSV(pets, "updated_attendance.csv"); // write modified data to a separate file. 
-    //Note, if the output file still exist in the path where the output file will be made, the modified data will instead be put in the old output data
+    writeCSV(pets, "updated_attendance.csv");
 
     return 0;
 }
+
